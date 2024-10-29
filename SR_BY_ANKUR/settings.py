95ce1089b6cef59db3f7e95ccf6393bffe1900b4
Ankursharma2003 <ankur.sharma@irbureau.com>
2024-10-29 12:22:23 +0530

    Add files via upload

--- SR_BY_ANKUR/settings.py
+++ SR_BY_ANKUR/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-4ymkj**x#=&af8odnb5_&97x3m2$gxtz3lruwsj*y9%0ki&1i*'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['.vercel.app']
 
